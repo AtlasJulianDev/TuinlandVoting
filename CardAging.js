e0072b49cf9d0413ee2073669b94dd0e47b949df
AtlasJulianDev <130748452+AtlasJulianDev@users.noreply.github.com>
2023-04-14 15:20:50 +0200

    comiet

--- CardAging.js
+++ CardAging.js
@@ -1,14 +1,12 @@
 // you can also use the t you get in a capability function
 var t = window.TrelloPowerUp.iframe();
 
-// simplest alert requires just a message
-// displays for 5 seconds using the 'info' display
+// show an alert for a bit, we don't know how long it will
+// take so we will hide it manually when its done
 t.alert({
-  message: 'Powering-Up, give us a second...'
+  message: 'Working on it, hang tight...',
+  duration: 30, // don't worry we will close it sooner
 });
 
-// more complex alert
-t.alert({
-  message: 'Powered-Up Successfully 🎉',
-  duration: 6,
-});
+// some time later once the operation is complete
+window.setTimeout(t.hideAlert, 8000);
