fbb29a5e73c1b768c9aba89b095ca65b6cd5a4c1
AtlasJulianDev <130748452+AtlasJulianDev@users.noreply.github.com>
2023-04-14 14:23:43 +0200

    Add files via upload

--- voting.js
+++ voting.js
@@ -6,34 +6,46 @@ var POLL_VOTE_CLASS = 'poll-vote';
 var POLL_VOTED_CLASS = 'poll-voted';
 
 // Create Poll Option
-function createPollOption(optionText) {
-  var pollOption = document.createElement('div');
-  pollOption.className = POLL_OPTION_CLASS;
+function createPollOption(optionText, voteCount) {
+    var pollOption = document.createElement('div');
+    pollOption.className = POLL_OPTION_CLASS;
+  
+    var pollVote = document.createElement('button');
+    pollVote.className = POLL_VOTE_CLASS;
+    pollVote.textContent = 'Vote';
+    pollVote.addEventListener('click', function() {
+      handlePollVote(pollOption);
+    });
+  
+    var pollText = document.createElement('span');
+    pollText.textContent = optionText;
+  
+    var pollVoteCount = document.createElement('span');
+    pollVoteCount.className = POLL_VOTE_COUNT_CLASS;
+    pollVoteCount.textContent = 'Votes: ' + voteCount;
+  
+    pollOption.appendChild(pollVote);
+    pollOption.appendChild(pollText);
+    pollOption.appendChild(pollVoteCount);
+  
+    return pollOption;
+  }
 
-  var pollVote = document.createElement('button');
-  pollVote.className = POLL_VOTE_CLASS;
-  pollVote.textContent = 'Vote';
-  pollVote.addEventListener('click', function() {
-    handlePollVote(pollOption);
-  });
-
-  var pollText = document.createElement('span');
-  pollText.textContent = optionText;
-
-  pollOption.appendChild(pollVote);
-  pollOption.appendChild(pollText);
-
-  return pollOption;
-}
+  function updatePollVoteCount(pollOption, voteCount) {
+    pollOption.querySelector('.' + POLL_VOTE_COUNT_CLASS).textContent = 'Votes: ' + voteCount;
+  }
 
 // Handle Poll Vote
 function handlePollVote(pollOption) {
-  if (!pollOption.classList.contains(POLL_VOTED_CLASS)) {
-    pollOption.classList.add(POLL_VOTED_CLASS);
-    pollOption.querySelector('.' + POLL_VOTE_CLASS).textContent = 'Voted';
-    // Perform additional actions when a vote is cast, e.g., update vote count, send data to backend, etc.
+    if (!pollOption.classList.contains(POLL_VOTED_CLASS)) {
+      pollOption.classList.add(POLL_VOTED_CLASS);
+      pollOption.querySelector('.' + POLL_VOTE_CLASS).textContent = 'Voted';
+      var voteCount = parseInt(pollOption.querySelector('.' + POLL_VOTE_COUNT_CLASS).textContent.split(' ')[1]);
+      voteCount++;
+      updatePollVoteCount(pollOption, voteCount);
+      // Perform additional actions when a vote is cast, e.g., update vote count, send data to backend, etc.
+    }
   }
-}
 
 // Handle Create Poll
 function handleCreatePoll(t) {
@@ -41,15 +53,15 @@ function handleCreatePoll(t) {
       title: 'Create Poll',
       items: [
         {
-          text: 'Poll Option 1',
+          text: 'Accept',
           callback: function(t) {
-            addPollOption(t, 'Poll Option 1');
+            addPollOption(t, 'Accept');
           }
         },
         {
-          text: 'Poll Option 2',
+          text: 'Decline',
           callback: function(t) {
-            addPollOption(t, 'Poll Option 2');
+            addPollOption(t, 'Decline');
           }
         }
         // Add more options as needed
@@ -70,25 +82,27 @@ function handleCreatePoll(t) {
   // Initialize Power-Up
   window.TrelloPowerUp.initialize({
     'card-badges': function(t, options) {
-        return t.card('id', 'name', 'desc').then(function(card) {
-          var pollOptions = options['poll-options'] || [];
-          var pollOptionElements = pollOptions.map(function(optionText) {
-            return createPollOption(optionText);
+        return t.card('id', 'name', 'desc') // Get card information
+          .then(function(card) {
+            var pollOptions = options['poll-options'] || [];
+            var pollOptionElements = pollOptions.map(function(optionText) {
+              var voteCount = options['poll-votes'][optionText] || 0; // Get vote count from shared board data
+              return createPollOption(optionText, voteCount);
+            });
+            return [{
+              icon: '',
+              text: '',
+              items: pollOptionElements
+            }];
           });
-          return [{
-            icon: '',
-            text: '',
-            items: pollOptionElements
-          }];
-        });
-    },
-    'card-buttons': function(t, options) {
-      return [{
-        text: 'Create Poll',
-        callback: function(t) {
-          handleCreatePoll(t);
-        }
-      }];
-    }
+      },
+      'card-buttons': function(t, options) {
+        return [{
+          text: 'Create Poll',
+          callback: function(t) {
+            handleCreatePoll(t);
+          }
+        }];
+      }
   });
   
